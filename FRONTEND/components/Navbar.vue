64679a50c8c988fa14140d2f1ddbfaa4134d3f4f
6-ON <aminhatim6@gmail.com>
2023-03-15 00:09:17 +0100

    create navbar

---
+++ FRONTEND/components/Navbar.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const collapsed = useState('sidebarColapsed')
+</script>
+
+<template>
+  <div class="fixed flex justify-end top-0 left-0 items-center px-4 bg-brown-1 text-brown-4 w-full z-10 h-16">
+    <button class="mr-auto" @click="collapsed = !collapsed">
+      <IconsMenu class="w-8" />
+    </button>
+    
+    <div class="flex gap-3">
+      <IconsLibraryAdd class="w-8" />
+      <UserAvatar></UserAvatar>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
