3035d3e14765d54b4830893effe38c93f4949d43
6-ON <aminhatim6@gmail.com>
2023-03-15 00:10:47 +0100

    create default layout

---
+++ FRONTEND/layouts/default.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="bg-brown-1">
+    <Sidebar></Sidebar>
+    <Navbar></Navbar>
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
