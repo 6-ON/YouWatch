c50097d28ebd0237abf984df9f74aced7e14bfc7
6-ON <aminhatim6@gmail.com>
2023-03-15 00:10:33 +0100

    add transitions

---
+++ FRONTEND/components/transitions/Fade.vue
@@ -0,0 +1,16 @@
+<template>
+    <Transition name="shrink">
+        <slot />
+    </Transition>
+</template>
+<style scoped>
+.shrink-enter-active,
+.shrink-leave-active {
+    transition: opacity 0.1s;
+}
+
+.shrink-enter,
+.shrink-leave-to {
+    opacity: 0;
+}
+</style>
