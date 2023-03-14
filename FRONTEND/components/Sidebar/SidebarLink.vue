55a43884d7baa91bac2cd13587c8808e83671ded
6-ON <aminhatim6@gmail.com>
2023-03-15 00:10:19 +0100

    add sidebar link

---
+++ FRONTEND/components/Sidebar/SidebarLink.vue
@@ -0,0 +1,14 @@
+
+<script setup>
+const props = defineProps({
+    'to': { required: true },
+})
+
+</script>
+<template>
+    <NuxtLink exact-active-class="[&>div.circle-mark]:bg-[currentColor] bg-brown-3 text-brown-1" :to="to"
+        class="flex items-center py-2 justify-around w-full  relative">
+        <div class="circle-mark h-full aspect-square left-0 rounded-full -translate-x-[50%] absolute"></div>
+        <slot />
+    </NuxtLink>
+</template>
