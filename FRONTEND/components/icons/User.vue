eeda290e1a869095afdea686343353ebb0be5a79
6-ON <aminhatim6@gmail.com>
2023-03-15 00:09:59 +0100

    add icons

---
+++ FRONTEND/components/icons/User.vue
@@ -0,0 +1,7 @@
+<template>
+    <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <path clip-rule="evenodd" fill-rule="evenodd"
+            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-5.5-2.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 12a5.99 5.99 0 00-4.793 2.39A6.483 6.483 0 0010 16.5a6.483 6.483 0 004.793-2.11A5.99 5.99 0 0010 12z">
+        </path>
+    </svg>
+</template>
