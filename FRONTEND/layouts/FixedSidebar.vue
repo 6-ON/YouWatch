df18e9b45f32fd48c1de10f1d049de1ba2ea16b8
6-ON <aminhatim6@gmail.com>
2023-03-17 20:05:56 +0100

    add fixed sidebar layout

---
+++ FRONTEND/layouts/FixedSidebar.vue
@@ -0,0 +1,17 @@
+<script setup>
+const sidebar = useSidebar()
+onMounted(() => {
+    sidebar.setLocked()
+})
+onUnmounted(() => {
+    sidebar.setUnlocked()
+})
+</script>
+
+<template>
+    <div class="pt-16 pl-[20rem]">
+        <Sidebar></Sidebar>
+        <Navbar></Navbar>
+        <slot />
+    </div>
+</template>
