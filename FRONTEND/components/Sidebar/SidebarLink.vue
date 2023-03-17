a760d746f0f2ac290eb0a97ecd051bad8ef372f5
6-ON <aminhatim6@gmail.com>
2023-03-17 15:13:03 +0100

    enhance logic

--- FRONTEND/components/Sidebar/SidebarLink.vue
+++ FRONTEND/components/Sidebar/SidebarLink.vue
@@ -3,12 +3,14 @@
 const props = defineProps({
     'to': { required: true },
 })
-
+const sidebar = useSidebar()
 </script>
 <template>
-    <NuxtLink exact-active-class="[&>div.circle-mark]:bg-[currentColor] bg-brown-3 text-brown-1" :to="to"
-        class="flex items-center py-2 justify-around w-full  relative">
-        <div class="circle-mark h-full aspect-square left-0 rounded-full -translate-x-[50%] absolute"></div>
-        <slot />
-    </NuxtLink>
+    <div class="w-full" @click="sidebar.toggle">
+        <NuxtLink exact-active-class="[&>div.circle-mark]:bg-[currentColor] bg-brown-3 text-brown-1" :to="to"
+            class="flex items-center py-2 justify-around w-full  relative">
+            <div class="circle-mark h-full aspect-square left-0 rounded-full -translate-x-[50%] absolute"></div>
+            <slot />
+        </NuxtLink>
+    </div>
 </template>
