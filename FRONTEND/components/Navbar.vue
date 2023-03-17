02a0c3091e44b341331871864aeb653a89eaf712
6-ON <aminhatim6@gmail.com>
2023-03-17 15:20:38 +0100

    enhance logic

--- FRONTEND/components/Navbar.vue
+++ FRONTEND/components/Navbar.vue
@@ -1,13 +1,12 @@
 <script lang="ts" setup>
-const collapsed = useState('sidebarColapsed')
+const sidebar = useSidebar()
 </script>
 
 <template>
   <div class="fixed flex justify-end top-0 left-0 items-center px-4 bg-brown-1 text-brown-4 w-full z-10 h-16">
-    <button class="mr-auto" @click="collapsed = !collapsed">
+    <button class="mr-auto" @click="sidebar.toggle">
       <IconsMenu class="w-8" />
     </button>
-    
     <div class="flex gap-3">
       <IconsLibraryAdd class="w-8" />
       <UserAvatar></UserAvatar>
