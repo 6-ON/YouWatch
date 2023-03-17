b160a871909c010d604d20012bcc8e79e60dc41f
6-ON <aminhatim6@gmail.com>
2023-03-17 19:42:07 +0100

    add animate css

--- FRONTEND/nuxt.config.ts
+++ FRONTEND/nuxt.config.ts
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     css: [
-        '@/assets/style/fonts.css'
+        '@/assets/style/fonts.css',
+        'animate.css/animate.min.css'
     ],
     modules :['@nuxtjs/tailwindcss', '@vueuse/nuxt']
 })
