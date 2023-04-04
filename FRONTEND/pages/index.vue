cba2aec1f8af266114c4ca2959eb514d0ff93b83
6-ON <aminhatim6@gmail.com>
2023-04-04 18:30:48 +0000

    Changes to FRONTEND/pages/index.vue

--- FRONTEND/pages/index.vue
+++ FRONTEND/pages/index.vue
@@ -3,23 +3,25 @@ useHead({
   title: 'Home'
 })
 definePageMeta({ layout: 'fixed-sidebar' });
+const videos = ref([])
+onMounted(() => {
+  setTimeout(() => {
+    videos.value = Array(10)
+  }, 1000);
+})
+
 </script>
 
 <template>
-  <TransitionGroup name="fade-in" class="grid grid-cols-1 gap-y-4 xl:grid-cols-2 2xl:grid-cols-3 place-items-center p-4 bg-brown-1" tag="div" appear>
-      <VideoThumbnail v-for="n in 10" :key="n" ></VideoThumbnail>
-
-  </TransitionGroup>
+  <div>
+    <CategoryShips />
+    <TransitionGroup v-if="!videos.length"
+      class="grid grid-cols-1 gap-y-4 xl:grid-cols-2 2xl:grid-cols-3 place-items-center p-4" tag="div">
+      <PlaceholderVideoThumbnail  class="w-96" v-for="(n, index) in 10" :key="index"></PlaceholderVideoThumbnail>
+    </TransitionGroup>
+    <TransitionGroup v-else
+      class="grid grid-cols-1 gap-y-4 xl:grid-cols-2 2xl:grid-cols-3 place-items-center p-4" tag="div">
+      <VideoThumbnail class="w-96" v-for="(n, index) in videos" :key="index"></VideoThumbnail>
+    </TransitionGroup>
+  </div>
 </template>
-
-<style >
-.fade-in-enter-active,
-.fade-in-leave-active {
-  transition: opacity 1s;
-}
-
-.fade-in-enter,
-.fade-in-leave-to {
-  opacity: 0;
-}
-</style>
