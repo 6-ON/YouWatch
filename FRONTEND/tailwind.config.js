4fce253c290ba22aa9023aa679206423a821d7ce
6-ON <aminhatim6@gmail.com>
2023-04-04 18:31:54 +0000

    Changes to FRONTEND/tailwind.config.js

--- FRONTEND/tailwind.config.js
+++ FRONTEND/tailwind.config.js
@@ -3,17 +3,30 @@ module.exports = {
   content: [],
   theme: {
     extend: {
-      colors:{
-        brown:{
-          1:'#FFF8EA',
-          2:'#9E7676',
-          3:'#815B5B',
-          4:'#594545',
+      colors: {
+        brown: {
+          1: '#FFF8EA',
+          2: '#9E7676',
+          3: '#815B5B',
+          4: '#594545',
         }
       },
       fontFamily: {
         Montserrat: ['Montserrat', 'sans-serif'],
         Inter: ['Inter', 'sans-serif'],
+      },
+      animation: {
+        'pop-in': 'popIn 0.1s',
+        'pop-out': 'popOut 0.1s',
+      }, keyframes: {
+        'popIn': {
+          '0%': {transform: 'scale(0)'},
+          '100%': {transform: 'scale(1)'},
+        },
+        'popOut': {
+          '0%': {transform: 'scale(1)'},
+          '100%': {transform: 'scale(0)'},
+        },
       }
     },
   },
