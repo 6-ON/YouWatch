dc5043441e9a7e562a885eabee9f7e8013d4fab7
6-ON <aminhatim6@gmail.com>
2023-04-04 18:27:25 +0000

    Changes to FRONTEND/components/Modal/Spinner.vue

---
+++ FRONTEND/components/Modal/Spinner.vue
@@ -0,0 +1,11 @@
+<template>
+    <!-- loading spinner Modal -->
+    <div class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50">
+        <div class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-brown-1 w-96 h-96 rounded-lg z-50">
+            <div class="flex justify-center items-center h-full">
+                <div class="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+            </div>
+        </div>
+    </div>
+
+</template>
