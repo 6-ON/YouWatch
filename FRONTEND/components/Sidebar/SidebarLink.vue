f4acebdc845e27ee6944085be1737b09cbc02171
6-ON <aminhatim6@gmail.com>
2023-04-04 18:27:46 +0000

    Changes to FRONTEND/components/Sidebar/SidebarLink.vue

--- FRONTEND/components/Sidebar/SidebarLink.vue
+++ FRONTEND/components/Sidebar/SidebarLink.vue
@@ -2,15 +2,23 @@
 <script setup>
 const props = defineProps({
     'to': { required: true },
+    'icon': { required: true, type: String },
 })
+
 const sidebar = useSidebar()
 </script>
 <template>
     <div class="w-full" @click="sidebar.hide">
-        <NuxtLink exact-active-class="[&>div.circle-mark]:bg-[currentColor] bg-brown-3 text-brown-1" :to="to"
-            class="flex items-center py-2 justify-around w-full  relative">
-            <div class="circle-mark h-full aspect-square left-0 rounded-full -translate-x-[50%] absolute"></div>
-            <slot />
+        <NuxtLink exact-active-class="bg-brown-3 text-brown-1" :to="to"
+            class="flex items-center py-2 gap-2 w-full relative "
+            :class="{ 'pl-8': !sidebar.collapsed(),'justify-center': sidebar.collapsed() }">
+
+            <span class="material-symbols-sharp">
+                {{ icon }}
+            </span>
+            <span v-if="!sidebar.collapsed()">
+                <slot />
+            </span>
         </NuxtLink>
     </div>
 </template>
