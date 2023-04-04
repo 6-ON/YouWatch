aac1eee1a1f4ec917f72302a314aeb53b6c65ea3
6-ON <aminhatim6@gmail.com>
2023-04-04 18:28:25 +0000

    Changes to FRONTEND/components/navbar/LogoMenu.vue

---
+++ FRONTEND/components/navbar/LogoMenu.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex items-center gap-16">
+        <NavbarMenuToggle />
+        <NuxtLink to="/"><img class="w-14" src="@/assets/logo-icon.png"></NuxtLink>
+    </div>
+</template>
