96eb5190831fa0d2e229979029afaff0f1dc259c
6-ON <aminhatim6@gmail.com>
2023-04-04 18:28:05 +0000

    Changes to FRONTEND/components/common/Search.vue

---
+++ FRONTEND/components/common/Search.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="relative">
+        <input type="text" name="" class="rounded-3xl p-2 pl-10  border-2 w-80 bg-brown-1 border-brown-4 border-opacity-30 focus:border-opacity-100 focus:outline-none placeholder:text-brown-2 placeholder:text-opacity-60 " placeholder="Search for Video">
+        <span class="material-symbols-sharp absolute left-1 top-[50%] -translate-y-[50%] ">
+            search
+        </span>
+    </div>
+</template>
