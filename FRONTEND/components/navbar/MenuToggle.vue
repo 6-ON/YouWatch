03190fa043a116ee1054ac6c2262c76eb4ed0a0c
6-ON <aminhatim6@gmail.com>
2023-04-04 18:28:32 +0000

    Changes to FRONTEND/components/navbar/MenuToggle.vue

---
+++ FRONTEND/components/navbar/MenuToggle.vue
@@ -0,0 +1,10 @@
+<script setup>
+const sidebar = useSidebar()
+</script>
+<template>
+    <div>
+        <button @click="sidebar.toggle">
+            <IconsMenu class="w-8" />
+        </button>
+    </div>
+</template>
