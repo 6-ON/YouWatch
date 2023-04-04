91be930a37fb9e735e6a34435dfb0b4862a46e93
6-ON <aminhatim6@gmail.com>
2023-04-04 18:30:55 +0000

    Changes to FRONTEND/pages/library.vue

--- FRONTEND/pages/library.vue
+++ FRONTEND/pages/library.vue
@@ -1,14 +1,19 @@
 <script setup>
 useHead({
-  title : 'Library'
+  title: 'Library'
 })
+definePageMeta({ layout: 'fixed-sidebar' })
 </script>
 
 
 
 <template>
-  <div>
-    Page: Video
+  <div class="bg-brown-1 text-brown-4 p-2">
+    <CommonSection title="History" class="p-4">
+      <div class="flex flex-wrap gap-1 transition-all duration-100 justify-center ">
+        <VideoThumbnail class="w-56 self-baseline" v-for="i in 10" :with-avatar="false"/>
+      </div>
+    </CommonSection>
   </div>
 </template>
 
