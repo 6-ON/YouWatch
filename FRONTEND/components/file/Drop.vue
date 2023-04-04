bc8638e746e8bc85261c5c7f40b16c60defc7180
6-ON <aminhatim6@gmail.com>
2023-04-04 18:28:18 +0000

    Changes to FRONTEND/components/file/Drop.vue

---
+++ FRONTEND/components/file/Drop.vue
@@ -0,0 +1,24 @@
+<script setup>
+const emits = defineEmits(['selected'])
+const props = defineProps({
+  targetId: {
+    required: true,
+    type: String
+  }
+})
+</script>
+
+<template>
+  <div class="bg-brown-2 bg-opacity-20 select-none font-Montserrat font-medium flex justify-center items-center aspect-video mx-auto gap-3 rounded-2xl"
+    @drop.prevent="(e) => $emit('selected', e.dataTransfer.files[0])" @dragenter.prevent @dragover.prevent>
+    <span class="material-symbols-sharp">
+      upload
+    </span>
+    <span>Drop your file here or </span>
+    <label :for="targetId" class="bg-brown-1 p-2 rounded-full cursor-pointer  animate-pulse">
+      Select a File
+    </label>
+  </div>
+</template>
+
+<style scoped></style>
