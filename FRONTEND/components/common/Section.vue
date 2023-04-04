8eeb2620d93988bdd946004d66a526f0a1348917
6-ON <aminhatim6@gmail.com>
2023-04-04 18:28:12 +0000

    Changes to FRONTEND/components/common/Section.vue

---
+++ FRONTEND/components/common/Section.vue
@@ -0,0 +1,10 @@
+<script setup>
+const props = defineProps(['title'])
+</script>
+
+<template>
+    <div class="relative rounded-lg border border-brown-3 pt-12 ">
+        <h2 class="absolute left-0 top-0 rounded-br-lg rounded-tl-lg bg-brown-3 text-brown-1 px-2 py-0.5">{{ title }}</h2>
+        <slot/>
+    </div>
+</template>
