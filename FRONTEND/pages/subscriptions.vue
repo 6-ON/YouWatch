c482fdb5541c7aedec2a5d08c138cfac587173ce
6-ON <aminhatim6@gmail.com>
2023-04-04 18:31:20 +0000

    Changes to FRONTEND/pages/subscriptions.vue

---
+++ FRONTEND/pages/subscriptions.vue
@@ -0,0 +1,12 @@
+<script setup>
+useHead({
+    title: 'subscriptions'
+})
+definePageMeta({ layout: 'fixed-sidebar' })
+</script>
+<template>
+    <div class="bg-brown-1 flex flex-wrap justify-center gap-1 p-4">
+        <VideoThumbnail v-for="n in 20" class="w-56"  :with-avatar="false" />
+    </div>
+
+</template>
