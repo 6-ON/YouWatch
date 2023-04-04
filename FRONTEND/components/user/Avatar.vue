b5c511620f0770d6b7f59708ab704e69fb7077a2
6-ON <aminhatim6@gmail.com>
2023-04-04 18:28:53 +0000

    Changes to FRONTEND/components/user/Avatar.vue

--- FRONTEND/components/user/Avatar.vue
+++ FRONTEND/components/user/Avatar.vue
@@ -1,14 +1,18 @@
 <script setup>
     const props = defineProps({
         image:{
+            type:String,
+            required:true
+        },
+        size:{
+            default:'w-12 h-12',
             type:String
         }
     })
 </script>
 
 <template>
-    <div>
-        <img v-if="image" class="w-12 h-12 ring-4 ring-brown-3 rounded-full"  :src="image"/>
-        <IconsUser v-else class="w-12"/>
+    <div class="min-w-fit min-h-fit">
+        <img :class="size" class="ring-4 ring-brown-3 rounded-full"  :src="image"/>
     </div>
 </template>
