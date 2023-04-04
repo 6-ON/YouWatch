c30726820597eaee6711833be24df431ed477152
6-ON <aminhatim6@gmail.com>
2023-04-04 18:27:38 +0000

    Changes to FRONTEND/components/Sidebar/Sidebar.vue

--- FRONTEND/components/Sidebar/Sidebar.vue
+++ FRONTEND/components/Sidebar/Sidebar.vue
@@ -1,40 +1,43 @@
 <script setup>
 const sidebar = useSidebar()
+const stateClass = computed(() => {
+	if (!sidebar.collapsed()) {
+		return 'w-[15rem]'
+	}
+	else if (sidebar.locked()) {
+		return 'w-[4rem]'
+	} else {
+		return 'w-0'
+	}
+})
 </script>
 
 <template>
 	<div>
 		<TransitionsFade>
-			<div class="w-full h-screen bg-black bg-opacity-20 fixed top-0 left-0 z-20 backdrop:blur" v-if="!sidebar.isLocked() && !sidebar.isCollapsed()"
-				@click="sidebar.hide"></div>
+			<div class="w-full h-screen bg-black bg-opacity-20 fixed top-0 left-0 z-10 backdrop:blur"
+				v-if="!sidebar.locked() && !sidebar.collapsed()" @click="sidebar.hide"></div>
 		</TransitionsFade>
-		<div class="z-20 fixed top-0 h-screen bg-brown-2 flex flex-col items-center py-10 transition-all duration-100 w-[20rem] text-brown-4 font-Montserrat font-semibold "
-			:class="{ 'left-0': !sidebar.isCollapsed(), '-left-[20rem]': sidebar.isCollapsed() }">
-			<div class="flex items-center mb-16 justify-center w-full gap-2">
-				<NuxtLink to="/"><img class="w-20" src="@/assets/logo-icon.png"></NuxtLink>
-			</div>
+
+		<div class="fixed z-20 top-0 left-0 h-screen bg-brown-1 flex flex-col items-center pb-10 pt-20  text-brown-4 font-Montserrat font-semibold overflow-x-hidden "
+			:class="stateClass">
+
 			<div class="flex flex-col items-center gap-2 w-full antialiased">
-				<SidebarLink to="/" >
-					<span class="material-symbols-sharp text-4xl ">
-						home
-					</span>
-					<span>Home</span>
+				<SidebarLink to="/" icon="home">
+					Home
 				</SidebarLink>
 
-				<SidebarLink to="/library" >
-					<span class="material-symbols-sharp">
-						video_library
-					</span>
-					<span>Library</span>
+				<SidebarLink to="/library" icon="video_library">
+					Library
+				</SidebarLink>
+				<SidebarLink to="/subscriptions" icon="subscriptions">
+					Subscriptions
 				</SidebarLink>
 			</div>
 
 			<div class="w-full mt-auto">
-				<SidebarLink to="/settings" icon="IconsHome">
-					<span class="material-symbols-sharp">
-						settings
-					</span>
-					<span>Settings</span>
+				<SidebarLink to="/settings" icon="settings">
+					Settings
 				</SidebarLink>
 			</div>
 		</div>
