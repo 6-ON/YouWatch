4156dda372e6d0bd7d9eb489342968a5c0bc8e5a
6-ON <aminhatim6@gmail.com>
2023-04-04 18:26:52 +0000

    Changes to BACKEND/database/migrations/2014_10_12_000000_create_users_table.php

--- BACKEND/database/migrations/2014_10_12_000000_create_users_table.php
+++ BACKEND/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
+            $table->string('image')->default('https://i0.wp.com/researchictafrica.net/wp/wp-content/uploads/2016/10/default-profile-pic.jpg?ssl=1');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('isAdmin')->default(false);
+
             $table->rememberToken();
             $table->timestamps();
         });
