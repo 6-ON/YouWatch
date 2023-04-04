24e286acb75f8641bbd882af6011de6385f17471
6-ON <aminhatim6@gmail.com>
2023-04-04 18:29:24 +0000

    Changes to FRONTEND/composables/useSidebar.ts

--- FRONTEND/composables/useSidebar.ts
+++ FRONTEND/composables/useSidebar.ts
@@ -1,29 +1,35 @@
 export const useSidebar = () => {
-  const state = useState('sidebarState', () => {
-    return {
-      collapsed: true,
-      locked: false,
-    }
-  })
-  const isCollapsed = () => state.value.collapsed
-  const isLocked = () => state.value.locked
+  const state = useState('sidebarState', () => ({
+    collapsed: true,
+    locked: false,
+  }))
+  const collapsed = () => state.value.collapsed
+  const locked = () => state.value.locked
 
+  const toggle = () => {
+      state.value.collapsed = !collapsed()
+  }
   const hide = () => {
-    if (!state.value.locked) {
       state.value.collapsed = true
-    }
   }
   const show = () => {
     state.value.collapsed = false
   }
-  const setLocked = () => {
-    state.value.collapsed = false
+  const lock = () => {
     state.value.locked = true
   }
-  const setUnlocked = () => {
-    state.value.collapsed = true
+  const unlock = () => {
     state.value.locked = false
   }
 
-  return { state, hide, show, setLocked, isCollapsed, isLocked, setUnlocked }
+  return {
+    state,
+    hide,
+    show,
+    toggle,
+    lock,
+    collapsed,
+    locked,
+    unlock
+  }
 }
