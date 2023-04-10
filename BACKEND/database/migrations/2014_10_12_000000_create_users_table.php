1179914b59ea891e5244dc8c02997ce04fbfc2b4
6-ON <aminhatim6@gmail.com>
2023-04-10 11:39:58 +0000

    update 2014_10_12_000000_create_users_table.php

--- BACKEND/database/migrations/2014_10_12_000000_create_users_table.php
+++ BACKEND/database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,6 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('isAdmin')->default(false);
-
             $table->rememberToken();
             $table->timestamps();
         });
