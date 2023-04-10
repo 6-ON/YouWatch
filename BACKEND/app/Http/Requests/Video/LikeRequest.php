66fcbb42d6fdddefaf129c007958973075a38384
6-ON <aminhatim6@gmail.com>
2023-04-10 22:05:28 +0000

    feat: create LikeRequest.php

---
+++ BACKEND/app/Http/Requests/Video/LikeRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Video;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LikeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        if ($this->video->likes()->where('user_id', auth()->id())->exists()) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'like' => 'required|boolean'
+        ];
+    }
+}
