0cc98ceb0dad06d0562377bc68e19e61f173416f
6-ON <aminhatim6@gmail.com>
2023-04-10 22:05:42 +0000

    update Comment.php

--- BACKEND/app/Models/Comment.php
+++ BACKEND/app/Models/Comment.php
@@ -8,7 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
-    
+    protected $guarded = [];
+    protected $hidden = ['user_id', 'video_id'];
     public function user()
     {
         return $this->belongsTo(User::class);
