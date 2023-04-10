0f719411e6f2a10ec6db1ca900dc9472adddd67f
6-ON <aminhatim6@gmail.com>
2023-04-10 21:56:21 +0000

    update CommentController.php

--- BACKEND/app/Http/Controllers/CommentController.php
+++ BACKEND/app/Http/Controllers/CommentController.php
@@ -3,40 +3,46 @@
 namespace App\Http\Controllers;
 
 use App\Models\Comment;
+use App\Models\Video;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Video $video)
     {
-        //
+        // validate request
+        $request->validate([
+            'body' => 'required|string'
+        ]);
+        // store comment
+        $video->comments()->create([
+            'user_id' => auth()->id(),
+            'body' => $request->body
+        ]);
+        // return created response code
+        return response()->json([
+            'message' => 'Comment created successfully'
+        ], 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Comment $comment)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, Comment $comment)
     {
-        //
+        // validate request
+        $request->validate([
+            'body' => 'required|string'
+        ]);
+        // update comment
+        $comment->update([
+            'body' => $request->body
+        ]);
     }
 
     /**
@@ -44,6 +50,7 @@ class CommentController extends Controller
      */
     public function destroy(Comment $comment)
     {
-        //
+        // delete comment
+        $comment->delete();
     }
 }
