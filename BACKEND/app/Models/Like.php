61df674255391d3f03dfea848c7bd19027d005ae
6-ON <aminhatim6@gmail.com>
2023-04-10 22:06:05 +0000

    update Like.php

--- BACKEND/app/Models/Like.php
+++ BACKEND/app/Models/Like.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class Like extends Model
+class Like extends Pivot
 {
     use HasFactory;
+    protected $guarded = [];
+    protected $table = 'likes';
     public function user()
     {
         return $this->belongsTo(User::class);
