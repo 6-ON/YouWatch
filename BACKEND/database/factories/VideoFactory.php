865468149ebaf26cec1475fe16b71f7c611cbe8d
6-ON <aminhatim6@gmail.com>
2023-04-10 22:16:37 +0000

    update VideoFactory.php

--- BACKEND/database/factories/VideoFactory.php
+++ BACKEND/database/factories/VideoFactory.php
@@ -19,8 +19,8 @@ class VideoFactory extends Factory
         return [
             'user_id' => \App\Models\User::factory(),
             'title' => fake()->sentence(),
-            'description' => fake()->paragraph(),
-            'tags' => fake()->words(3, true),
+            'description' => fake()->paragraph(2),
+            'tags' => [fake()->word(), fake()->word(), fake()->word()],
             'duration' => fake()->time('i:s'),
             'url' => 'https://res.cloudinary.com/dgmmjsxbf/video/upload/v1673039464/samples/cld-sample-video.mp4',
             'thumbnail_url' => fake()->imageUrl(),
