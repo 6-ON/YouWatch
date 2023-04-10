b50d761b2b05d89457eecb8ce797e3a13a80168e
6-ON <aminhatim6@gmail.com>
2023-04-10 22:16:14 +0000

    update Video.php

--- BACKEND/app/Models/Video.php
+++ BACKEND/app/Models/Video.php
@@ -4,20 +4,36 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Tags\HasTags;
 
 class Video extends Model
 {
-    use HasFactory;
+    use HasFactory,HasTags;
 
-    public function user()
+    protected $guarded = [];
+    protected $with = ['user', 'tags'];
+    protected $withCount = ['likes', 'comments'];
+    // protected $hidden = ['user_id', 'tags.pivot'];
+
+    //boot method
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope(new Scopes\Tags);
+        static::addGlobalScope(new Scopes\Filter);
+    }
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(User::class);
     }
-    public function likes()
+    
+
+    public function likes(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
         return $this->hasMany(Like::class)->where('isLiked', true);
     }
-    public function comments()
+    public function comments(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
         return $this->hasMany(Comment::class);
     }
