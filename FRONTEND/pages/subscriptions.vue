3bff21aa55325228547c1fbe2d3288ca79e6b453
6-ON <aminhatim6@gmail.com>
2023-04-19 16:40:30 +0000

    Modify FRONTEND/pages/subscriptions.vue

--- FRONTEND/pages/subscriptions.vue
+++ FRONTEND/pages/subscriptions.vue
@@ -1,12 +1,16 @@
-<script setup>
+<script lang="ts" setup>
 useHead({
     title: 'subscriptions'
 })
 definePageMeta({ layout: 'fixed-sidebar' })
+
+const { $axios } = useNuxtApp()
+const { data: subscriptions, pending: subscriptionsPending } = useAsyncData<VideoList>('subscriptions', async () => (await $axios.get('/api/subscriptions')).data)
 </script>
 <template>
-    <div class="bg-brown-1 flex flex-wrap justify-center gap-1 p-4">
-        <VideoThumbnail v-for="n in 20" class="w-56"  :with-avatar="false" />
+    <div  class="bg-brown-1 flex flex-wrap justify-center gap-1 p-4">
+        <PlaceholderVideoThumbnail v-if="subscriptionsPending || !subscriptions" v-for="n in 4" class="w-56"/>
+        <VideoThumbnail v-else v-for="video in subscriptions.data" class="w-56"  :with-avatar="false" :video="video" />
     </div>
 
 </template>
