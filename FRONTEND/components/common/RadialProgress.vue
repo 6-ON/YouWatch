682c63fed789b6d289c289aa10bf80c4dc737ba4
6-ON <aminhatim6@gmail.com>
2023-04-19 16:40:27 +0000

    Modify FRONTEND/components/common/RadialProgress.vue

---
+++ FRONTEND/components/common/RadialProgress.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup></script>
+
+<template>
+    <div class="circular-progress">
+        <span class="relative text-4xl font-semibold text-brown-4">0%</span>
+    </div>
+</template>
+
+<style scoped>
+
+.circular-progress{
+  @apply relative h-64 w-64 rounded-full flex items-center justify-center bg-gradient-to-r from-red-400 to-pink-500;
+
+}
+.circular-progress::before{
+  @apply absolute h-52 w-52 rounded-full bg-white ;
+}
+
+</style>
