79a69551ab66f3dc5d2b66d3434ed85d97e03bad
6-ON <aminhatim6@gmail.com>
2023-04-19 16:40:29 +0000

    Modify FRONTEND/error_y.vue

---
+++ FRONTEND/error_y.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="h-screen w-full flex flex-col justify-center items-center bg-brown-1">
+    <h1 class="text-9xl font-extrabold text-brown-2 tracking-widest">{{ error?.statusCode }}</h1>
+    <div class="bg-red-400 text-brown-1 px-2 text-sm rounded rotate-12 absolute">
+      {{ error?.statusMessage }}
+    </div>
+    <button class="mt-5">
+      <NuxtLink to="/" class="relative inline-block text-sm font-medium text-brown-1 group focus:outline-none focus:ring">
+        <span
+          class="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-brown-2 group-hover:translate-y-0 group-hover:translate-x-0"></span>
+
+        <span class="relative block px-8 py-3 bg-brown-4 border border-brown-2">
+          <button @click="goHome()" to="/">Go Home</button>
+        </span>
+      </NuxtLink>
+    </button>
+  </main>
+</template>
+  
+<script setup>
+const error = useError()
+function goHome() {
+  navigateTo('/')
+  // clearError({ redirect: '/' })
+}
+</script>
+  
