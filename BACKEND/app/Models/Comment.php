abcc069c7fe50f100f2937a0223ae29bc88f3b33
6-ON <aminhatim6@gmail.com>
2023-04-19 16:40:23 +0000

    Modify BACKEND/app/Models/Comment.php

--- BACKEND/app/Models/Comment.php
+++ BACKEND/app/Models/Comment.php
@@ -10,6 +10,7 @@ class Comment extends Model
     use HasFactory;
     protected $guarded = [];
     protected $hidden = ['user_id', 'video_id'];
+    protected $with = ['user:id,name,username,image'];
     public function user()
     {
         return $this->belongsTo(User::class);
