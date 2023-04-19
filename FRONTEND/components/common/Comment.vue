75373e720fa277f294603ee0713ffcd6f580c4a2
6-ON <aminhatim6@gmail.com>
2023-04-19 16:40:26 +0000

    Modify FRONTEND/components/common/Comment.vue

---
+++ FRONTEND/components/common/Comment.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  comment: CommentT
+}>()
+</script>
+
+<template>
+  <div class="flex gap-4">
+    <UserAvatar :image="comment.user.image" />
+    <div class="flex flex-col">
+      <div class="flex gap-1 items-center">
+        <h1 class="font-bold">{{ comment.user.name }}</h1>
+        <span class="text-xs text-gray-500">{{ useTimeAgo(new Date(comment.created_at)).value }}</span>
+      </div>
+      <p class="max-w-md text-sm">
+        {{ comment.body }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
