8cfa31fd3b5d6b474ada2a9f10e2b513a024138a
6-ON <aminhatim6@gmail.com>
2023-04-19 16:40:25 +0000

    Modify BACKEND/database/seeders/DatabaseSeeder.php

--- BACKEND/database/seeders/DatabaseSeeder.php
+++ BACKEND/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +15,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Video::factory(10)->create();
+        User::create([
+            'email' => 'amin@test.com',
+            'username' => 'amine',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'name' => 'amine',
+        ]);
 
+        $videos = json_decode(Storage::disk('local')->get('/data/videos.json'), true);
+        foreach ($videos as $video) {
+            \App\Models\Video::factory(1)->create([
+                'url' => $video['video'],
+                'thumbnail_url' => $video['thumbnail']
+            ]);
+        }
     }
 }
