d89d979dbe213d0acc57647bca8085afa3d538ce
6-ON <aminhatim6@gmail.com>
2023-04-19 16:40:30 +0000

    Modify FRONTEND/pages/library.vue

--- FRONTEND/pages/library.vue
+++ FRONTEND/pages/library.vue
@@ -1,8 +1,10 @@
-<script setup>
+<script lang="ts" setup>
 useHead({
   title: 'Library'
 })
 definePageMeta({ layout: 'fixed-sidebar' })
+const { $axios } = useNuxtApp()
+const { data: historyVideos, pending: historyPending } = useAsyncData<VideoList>('history', async () => (await $axios.get('/api/history')).data)
 </script>
 
 
@@ -11,7 +13,10 @@ definePageMeta({ layout: 'fixed-sidebar' })
   <div class="bg-brown-1 text-brown-4 p-2">
     <CommonSection title="History" class="p-4">
       <div class="flex flex-wrap gap-1 transition-all duration-100 justify-center ">
-        <VideoThumbnail class="w-56 self-baseline" v-for="i in 10" :with-avatar="false"/>
+        <PlaceholderVideoThumbnail class="w-64" v-if="historyPending" v-for="(n, index) in 3" :key="index" />
+        <VideoThumbnail class="w-56 self-baseline" v-else-if="historyVideos?.total" v-for="video in historyVideos.data"
+          :with-avatar="false" :video="video" />
+        <p v-else>Your history is empty enjoy watching videos</p>
       </div>
     </CommonSection>
   </div>
