5748159e05a5bcbf76486ec8548df084c85f8f8b
6-ON <aminhatim6@gmail.com>
2023-04-19 16:40:30 +0000

    Modify FRONTEND/pages/settings.vue

--- FRONTEND/pages/settings.vue
+++ FRONTEND/pages/settings.vue
@@ -2,12 +2,14 @@
 useHead({
     title:'settings'
 })
-
+const auth = useAuth()
 </script>
 <template>
     <div class="flex flex-col items-center">
         <div class="flex">
             
         </div>
+        <!-- logout button -->
+        <button class="bg-red-500 text-white p-2 rounded-lg mt-4" @click="auth.logout()">Logout</button>
     </div>
 </template>
