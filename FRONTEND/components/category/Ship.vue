26261574d7d26c7b7146a0ee5c4d89f4771958d4
6-ON <aminhatim6@gmail.com>
2023-04-19 16:40:26 +0000

    Modify FRONTEND/components/category/Ship.vue

--- FRONTEND/components/category/Ship.vue
+++ FRONTEND/components/category/Ship.vue
@@ -1,5 +1,11 @@
+<script setup>
+const props = defineProps(['isActive'])
+</script>
 <template>
-    <NuxtLink to="#" class="px-4 py-2 rounded-full border hover:bg-brown-2 hover:text-brown-1 border-brown-3 bg-brown-1">
-        hello
-    </NuxtLink>
+    <button :class="{
+        'bg-brown-1 hover:bg-brown-2 hover:text-brown-1': !isActive,
+        'bg-brown-2 text-brown-1': isActive
+    }" class="px-4 py-2 rounded-full border border-brown-3 ">
+        <slot />
+    </button>
 </template>
