8e0fde08f60ef10bc9998c0a53a1b5c9167eee12
6-ON <aminhatim6@gmail.com>
2023-04-21 00:12:36 +0000

    feat: add ProfileUpdateRequest

---
+++ BACKEND/app/Http/Requests/ProfileUpdateRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ProfileUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'username' => ['string',Rule::unique(User::class)->ignore($this->user()->id), 'max:255'],
+            'image' => ['string', 'url'],
+            'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+        ];
+    }
+}
