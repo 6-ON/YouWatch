a5ebc2f5aff0578049ccee1370de3bf98e20e71a
6-ON <aminhatim6@gmail.com>
2023-04-25 10:31:41 +0100

    feat : add middleware verifyAdmin

---
+++ BACKEND/app/Http/Middleware/verifyAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class verifyAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!auth()->user()->isAdmin) {
+            return response()->json([
+                'message' => 'You are not authorized to preform this action.',
+            ], 403);
+        }
+        return $next($request);
+    }
+}
